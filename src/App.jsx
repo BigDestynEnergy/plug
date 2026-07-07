af8ff0fe2f4aec5a5016a051b6b4f4b2c75c95c7
Destyn <destynbanda@gmail.com>
2026-07-07 13:10:54 +0200

    kaPlug no logs

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import { useState } from 'react'
 import './App.css'
 import AppRouter from './routes/App Router'
-import AuthRouter from "./routes/Auth Router"
 
 export default function App(){
   const [sign, setSign] = useState(!false);
 
   if(sign) return <AppRouter/>
- // else return(<AuthRouter/>)
 }
